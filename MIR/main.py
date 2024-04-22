aaeaa725cd370708fb6b5cdf7da9bee8c405db4a
Babis Kolokouris <72494172+kawaii-kitsune@users.noreply.github.com>
2024-04-22 17:02:54 +0300

    Add files via upload

---
+++ MIR/main.py
@@ -0,0 +1,10 @@
+# pip install librosa
+# pip install numpy
+# pip install muspy
+# pip install pretty_midi
+# pip install -U demucs
+# pip install joblib
+# pip install -U scikit-learn
+# pip install pandas
+# pip install pymusickit
+# pip install assemblyai
